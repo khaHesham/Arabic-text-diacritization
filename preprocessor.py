df9e3228003f79db27ab439d3a2087a3ffbbb3ec
khaled hesham <kha.hesham@gmail.com>
2023-12-29 18:21:56 +0200

    working on preprocessing

--- preprocessor.py
+++ preprocessor.py
@@ -6,7 +6,7 @@ class Preprocessor:
         with open('diacritic2id.pickle', 'rb') as file:
             # Load the object from the pickle file
             self.diacritic2id = pickle.load(file)
-            self.diacritic_classes = self.diacritic2id.keys()
+            self.diacritic_classes = list(self.diacritic2id.keys())
             
         with open('diacritics.pickle', 'rb') as file:
             self.diacritics = pickle.load(file)
@@ -20,7 +20,7 @@ class Preprocessor:
     def clean(self, corpus: str) -> str:
         separators = {' ', '.', ',', ';', ':'}
         allowed_chars = self.diacritics | self.arabic_letters | separators
-        pattern = f'[^{" ".join(allowed_chars)}]'
+        pattern = f'[^{"".join(allowed_chars)}]'
         cleaned_corpus = re.sub(pattern, '', corpus)
 
         return cleaned_corpus
@@ -37,6 +37,7 @@ class Preprocessor:
             if self.is_diacritic(char):
                 if(prev_is_diacritic):
                     curr_diacritic += char
+                    print(ord(curr_diacritic[0]), ord(curr_diacritic[1]))
                     diacritics[-1] = self.diacritic2id[curr_diacritic]
                 else:
                     diacritics.append(self.diacritic2id[char])
@@ -51,13 +52,25 @@ class Preprocessor:
         return characters, diacritics
 
 
-# try the clean method 
-corpus = '''وْلُهُ : ( أَوْ قَطَعَ الْأَوَّلُ يَدَهُ إلَخْ ) قَالَ الزَّرْكَشِيُّ( 14 / 123 )
-ابْنُ عَرَفَةَ : قَوْلُهُ : بِلَفْظٍ يَقْتَضِيه كَإِنْكَارِ غَيْرِ حَدِيثٍ بِالْإِسْلَامِ وُجُوبَ مَا عُلِمَ وُجُوبُهُ مِنْ الدِّينِ ضَرُورَةً ( كَإِلْقَاءِ مُصْحَفٍ بِقَذَرٍ وَشَدِّ زُنَّارٍ ) ابْنُ عَرَفَةَ : قَوْلُ ابْنِ شَاسٍ : أَوْ بِفِعْلٍ يَتَضَمَّنُهُ هُوَ كَلُبْسِ الزُّنَّارِ وَإِلْقَاءِ الْمُصْحَفِ فِي صَرِيحِ النَّجَاسَةِ وَالسُّجُودِ لِلصَّنَمِ وَنَحْوِ ذَلِكَ ( وَسِحْرٍ ) مُحَمَّدٌ : قَوْلُ مَالِكٍ وَأَصْحَابِهِ أَنَّ السَّاحِرَ كَافِرٌ بِاَللَّهِ تَعَالَى قَالَ مَالِكٌ : هُوَ كَالزِّنْدِيقِ إذَا عَمِلَ السِّحْرَ بِنَفْسِهِ قُتِلَ وَلَمْ يُسْتَتَبْ .
-( قَوْلُهُ لِعَدَمِ مَا تَتَعَلَّقُ إلَخْ ) أَيْ الْوَصِيَّةُ ( قَوْلُهُ مَا مَرَّ ) أَيْ قُبَيْلَ قَوْلِ الْمَتْنِ لَغَتْ وَلَوْ اقْتَصَرَ عَلَى أَوْصَيْت لَهُ بِشَاةٍ أَوْ أَعْطُوهُ شَاةً وَلَا غَنَمَ لَهُ عِنْدَ الْمَوْتِ هَلْ تَبْطُلُ الْوَصِيَّةُ أَوْ يُشْتَرَى لَهُ شَاةٌ وَيُؤْخَذُ مِنْ قَوْلِهِ الْآتِي كَمَا لَوْ لَمْ يَقُلْ مِنْ مَالِي وَلَا مِنْ غَنَمِي أَنَّهَا لَا تَبْطُلُ ، وَعِبَارَةُ الْكَنْزِ وَلَوْ لَمْ يَقُلْ مِنْ مَالِي وَلَا مِنْ غَنَمِي لَمْ يَتَعَيَّنْ غَنَمُهُ إنْ كَانَتْ انْتَهَتْ ا ه سم ( قَوْلُهُ فَيُعْطَى وَاحِدَةً مِنْهَا إلَخْ ) كَمَا لَوْ كَانَتْ مَوْجُودَةً عِنْدَ الْوَصِيَّةِ وَالْمَوْتِ ، وَلَا يَجُوزُ أَنْ يُعْطَى وَاحِدَةً مِنْ غَيْرِ غَنَمِهِ فِي الصُّورَتَيْنِ وَإِنْ تَرَاضَيَا ؛ لِأَنَّهُ صُلْحٌ عَلَى مَجْهُولٍ مُغْنِي وَنِهَايَةٌ قَالَ ع ش قَوْلُهُ وَاحِدَةً مِنْهَا أَيْ كَامِلَةً ، وَلَا يَجُوزُ أَنْ يُعْطَى نِصْفَيْنِ مِنْ شَاتَيْنِ ؛ لِأَنَّهُ لَا يُسَمَّى شَاةً وَقَوْلُهُ وَلَا يَجُوزُ أَنْ يُعْطَى وَاحِدَةً مِنْ غَيْرِ غَنَمِهِ وَيَنْبَغِي أَنْ يُقَالَ مِثْلُ ذَلِكَ فِي الْأَرِقَّاءِ اه .
-وَحَيَوَانٌ غَيْرُ مَوْجُودٍ .'''
 
-print(corpus)
+def main():
+    # Test the clean method
+    corpus = 'وَحَيَوَانٌ غَيْرُ مَوْجُودٍ'
+    preprocessor = Preprocessor()
 
-# preprocessor = Preprocessor()
-# print (preprocessor.clean(corpus))
+    # print(ord(preprocessor.diacritic_classes[12][0]), ord(preprocessor.diacritic_classes[12][1]))
+    
+    cleaned_corpus = preprocessor.clean(corpus)
+
+    characters, diacritics = preprocessor.separate_diacritics(cleaned_corpus)
+    print(characters)
+    print(diacritics)
+
+    # Test the separate_diacritics method
+    # sentence = "وَحَيَوَانٌ غَيْرُ مَوْجُودٍ ."
+    # characters, diacritics = preprocessor.separate_diacritics(sentence)
+    # print("Characters:", characters)
+    # print("Diacritics:", diacritics)
+
+if __name__ == "__main__":
+    main()
